137e745a5abda1e608fa9edd94dec33f1088828e
Juan Rodriguez <DeveloperJuanR@gmail.com>
2025-04-12 16:02:33 -0400

    Initial Commit

---
+++ Forkly/ForklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForklyApp.swift
+//  Forkly
+//
+//  Created by Juan Rodriguez on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ForklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
